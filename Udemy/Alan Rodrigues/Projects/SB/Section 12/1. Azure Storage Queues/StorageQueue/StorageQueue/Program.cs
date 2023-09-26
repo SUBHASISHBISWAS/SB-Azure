2563c6966a97dab1c6279457a260988465c5611e
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2023-09-26 09:31:21 +0530

    Pekk and Receive message from Queue

--- Udemy/Alan Rodrigues/Projects/SB/Section 12/1. Azure Storage Queues/StorageQueue/StorageQueue/Program.cs
+++ Udemy/Alan Rodrigues/Projects/SB/Section 12/1. Azure Storage Queues/StorageQueue/StorageQueue/Program.cs
@@ -1,12 +1,18 @@
 ﻿using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-string connectionString = "";
+string connectionString = "DefaultEndpointsProtocol=https;AccountName=saccalanrodrigues;AccountKey=ow7WEYyFRsfcy9imalNcMec2XBNtIuFxO5V4PyGmaT7avoXSNHU4mVBoLUKan0W2/WXWwS3ZOyaz+AStHdcwWw==;EndpointSuffix=core.windows.net";
 string queueName = "sbqueue";
 
-SendMessage("Hello Subhasish    ");
-void SendMessage(string message)
+//SendSimpleMessage("Hello Subhasish");
+
+//PeekSimpleMessage();
+
+//Console.WriteLine("Queue Length:: {0}",GetQueueLength());
+
+ReceiveSimpleMessage();
+
+void SendSimpleMessage(string message)
 {
     QueueClient queueClient = new QueueClient(connectionString, queueName);
 	if (queueClient.Exists())
@@ -15,3 +21,45 @@ void SendMessage(string message)
         Console.WriteLine("Message has been sent to Queue");
     }
 }
+
+void PeekSimpleMessage()
+{
+    QueueClient queueClient =new QueueClient(connectionString, queueName);
+    int maxMessage = 10;
+    if (queueClient.Exists())
+    {
+        PeekedMessage[] peekedMessages=queueClient.PeekMessages(maxMessage);
+        Console.WriteLine("In Queue Below message are Present");
+        foreach (var peekedMessage in peekedMessages) { 
+            Console.WriteLine(peekedMessage.Body); 
+        }
+    }
+}
+
+void ReceiveSimpleMessage()
+{
+    QueueClient queueClient = new QueueClient(connectionString, queueName);
+    int maxMessage = 10;
+    if (queueClient.Exists())
+    {
+        QueueMessage[] queueMessages = queueClient.ReceiveMessages(maxMessage);
+        Console.WriteLine("In Queue Below message are Present");
+        foreach (var queueMessage in queueMessages)
+        {
+            Console.WriteLine(queueMessage.Body);
+            queueClient.DeleteMessage(queueMessage.MessageId, queueMessage.PopReceipt);
+        }
+    }
+}
+
+int GetQueueLength()
+{
+    QueueClient queueClient = new QueueClient(connectionString, queueName);
+    if (queueClient.Exists())
+    {
+        QueueProperties properties = queueClient.GetProperties();
+        return properties.ApproximateMessagesCount;
+    }
+    return 0;
+}
+
