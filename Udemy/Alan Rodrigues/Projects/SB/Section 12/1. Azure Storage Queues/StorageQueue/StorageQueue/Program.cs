01583b461a24a776838471c5006c6f21063f2c44
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2023-09-26 08:57:37 +0530

    Message Send To Storage Queue

---
+++ Udemy/Alan Rodrigues/Projects/SB/Section 12/1. Azure Storage Queues/StorageQueue/StorageQueue/Program.cs
@@ -0,0 +1,17 @@
+﻿using Azure.Storage.Queues;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string connectionString = "";
+string queueName = "sbqueue";
+
+SendMessage("Hello Subhasish    ");
+void SendMessage(string message)
+{
+    QueueClient queueClient = new QueueClient(connectionString, queueName);
+	if (queueClient.Exists())
+	{
+		queueClient.SendMessage(message);
+        Console.WriteLine("Message has been sent to Queue");
+    }
+}
